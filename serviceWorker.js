aec3408cd80920461212d144007b7cf7b453cabd
webdevata <juramurodatamurod2023@gmail.com>
2023-06-23 05:25:37 -0700

    spinner change

---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+const staticSpinerWheel = "dev-spiner-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
